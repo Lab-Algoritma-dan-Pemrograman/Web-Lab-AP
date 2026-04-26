76fd5fd843b286c7ec8ddd6ebbc4c8dfd9a814aa
Luxey9 <a.faqodkurnia@gmail.com>
2026-04-26 15:10:51 +0700

    fix: remove stray character in Absensi.tsx

--- src/pages/Absensi.tsx
+++ src/pages/Absensi.tsx
@@ -825,7 +825,6 @@ export default function Absensi() {
                         ))
                       )}
                     </TableBody>
->
                   </Table>
                 </div>
               </CardContent>
